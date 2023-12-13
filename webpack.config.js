ac14632afe2e8b7aedfb28a40d3e925394a23675
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-12-13 13:09:33 -0600

    Config app

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const { webpackConfig, merge } = require('@rails/webpacker');
+
+module.exports = merge(webpackConfig, {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/application.js"
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+});
