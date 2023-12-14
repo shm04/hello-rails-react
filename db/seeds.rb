76493539dd153dd74407c7f622c105019bdc91f1
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-12-13 20:50:34 -0600

    Add seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Message.create(message: 'Hello world!')
+Message.create(message: 'Welcome to our community!')
+Message.create(message: 'Hello and thank you for joining!')
+Message.create(message: 'Greetings! Were glad youre here.')
+Message.create(message: 'Hi there! Welcome to the platform.')
